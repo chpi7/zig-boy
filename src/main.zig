616d496df3ff4d8016c84c434d691be983a98a92
chpi7 <11175879+chpi7@users.noreply.github.com>
2025-03-29 23:40:08 +0100

    draw simple gb model

--- src/main.zig
+++ src/main.zig
@@ -12,6 +12,105 @@ inline fn btoi(b: bool) u1 {
     return @intFromBool(b);
 }
 
+fn draw_gameboy(
+    px: i32,
+    py: i32,
+    smallRadius: i32,
+    bigRadius: i32,
+    innerWidth: i32,
+    innerHeight: i32,
+    render_screen_width: i32,
+) void {
+    // Inner Shell
+    rl.drawRectangle(
+        px + smallRadius,
+        py + smallRadius,
+        innerWidth,
+        innerHeight,
+        .red,
+    );
+    rl.drawRectangle(
+        px + smallRadius,
+        py,
+        innerWidth + (bigRadius - smallRadius),
+        smallRadius,
+        .red,
+    );
+    rl.drawRectangle(
+        px + smallRadius,
+        py + smallRadius + innerHeight,
+        innerWidth,
+        bigRadius,
+        .red,
+    );
+    rl.drawRectangle(
+        px,
+        py + smallRadius,
+        smallRadius,
+        innerHeight + (bigRadius - smallRadius),
+        .red,
+    );
+    rl.drawRectangle(
+        px + smallRadius + innerWidth,
+        py + smallRadius,
+        bigRadius,
+        innerHeight,
+        .red,
+    );
+
+    // Corners
+    rl.drawCircle(
+        px + smallRadius,
+        py + smallRadius,
+        @floatFromInt(smallRadius),
+        .red,
+    );
+    rl.drawCircle(
+        px + smallRadius,
+        py + smallRadius + innerHeight + (bigRadius - smallRadius),
+        @floatFromInt(smallRadius),
+        .red,
+    );
+    rl.drawCircle(
+        px + smallRadius + innerWidth + (bigRadius - smallRadius),
+        py + smallRadius,
+        @floatFromInt(smallRadius),
+        .red,
+    );
+    rl.drawCircle(
+        px + smallRadius + innerWidth,
+        py + smallRadius + innerHeight,
+        @floatFromInt(bigRadius),
+        .red,
+    );
+
+    const dpad_width = @as(f32, @floatFromInt(render_screen_width)) * 0.35 * 0.33;
+    const dpad_pos_x = @as(f32, @floatFromInt(innerWidth)) * 0.15;
+    const dpad_pos_y = @as(f32, @floatFromInt(innerHeight)) * 0.65;
+    // origin is top left, outside of both horizontal and vertical
+    // horizontal
+    rl.drawRectangleRounded(.{ .x = dpad_pos_x, .y = dpad_pos_y + dpad_width, .width = dpad_width * 3, .height = dpad_width }, 0.1, 10, .dark_gray);
+    // vertical
+    rl.drawRectangleRounded(.{ .x = dpad_pos_x + dpad_width, .y = dpad_pos_y, .width = dpad_width, .height = dpad_width * 3 }, 0.1, 10, .dark_gray);
+
+    // Buttons:
+    const btn_radius = @as(f32, @floatFromInt(bigRadius)) * 0.5;
+    const btn_pos_xa: i32 = @intFromFloat(@as(f32, @floatFromInt(innerWidth)) * 0.95 + btn_radius);
+    const btn_pos_xb: i32 = @intFromFloat(@as(f32, @floatFromInt(btn_pos_xa)) - 3 * btn_radius);
+    rl.drawCircle(
+        btn_pos_xa,
+        @intFromFloat(dpad_pos_y * 1.05),
+        btn_radius,
+        .dark_gray,
+    );
+    rl.drawCircle(
+        btn_pos_xb,
+        @intFromFloat(dpad_pos_y * 1.15),
+        btn_radius,
+        .dark_gray,
+    );
+}
+
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
@@ -34,28 +133,25 @@ pub fn main() !void {
     cpu.rf.AF = 0x0; // A = $11 indicates CGB.
     cpu.rf.PC = 0x0100;
 
-    // var thread_done = false;
-    // var gb_thread = try std.Thread.spawn(.{}, gameboy_fun, .{ &cpu, &thread_done });
-
-    // Raylib Initialization
-    //--------------------------------------------------------------------------------------
     const real_screen_width = 160;
     const real_screen_height = 144;
-    const screen_size_factor = 5;
+    const screen_size_factor = 3;
 
-    const screenWidth = real_screen_width * screen_size_factor;
-    const screenHeight = real_screen_height * screen_size_factor;
+    const render_screen_width = real_screen_width * screen_size_factor;
+    const render_screen_height = real_screen_height * screen_size_factor;
 
-    rl.initWindow(screenWidth, screenHeight, "raylib-zig [core] example - basic window");
+    const screenWidth: usize = @intFromFloat(render_screen_width * 1.5);
+    const screenHeight: usize = @intFromFloat(render_screen_height * 2.7);
+
+    rl.initWindow(screenWidth, screenHeight, "zig-boi");
     defer rl.closeWindow(); // Close window and OpenGL context
 
-    var fb_img = rl.genImageColor(screenWidth, screenHeight, rl.Color.dark_green);
+    var fb_img = rl.genImageColor(render_screen_width, render_screen_height, rl.Color.dark_green);
     fb_img.setFormat(.uncompressed_r8g8b8);
     const fb_tex = try rl.loadTextureFromImage(fb_img);
 
     rl.setTargetFPS(60);
     const cycles_per_frame = 17476; // = 1048576 / 60;
-    // const cycles_per_frame = 500; // = 1048576 / 60;
 
     while (!rl.windowShouldClose()) {
         // 1) set inputs
@@ -90,11 +186,35 @@ pub fn main() !void {
                 rl.imageDrawRectangle(&fb_img, x, y, s, s, rl.getColor(0x000000ff | c << 16));
             }
         }
-
         rl.updateTexture(fb_tex, fb_img.data);
-        rl.drawTexture(fb_tex, 0, 0, rl.Color.white);
-        rl.drawFPS(0, 0);
-    }
 
-    // gb_thread.join();
+        const px = 0;
+        const py = 0;
+        const smallRadius = 10 * screen_size_factor;
+        const bigRadius = 30 * screen_size_factor;
+        const innerWidth = screenWidth - (smallRadius + bigRadius);
+        const innerHeight = screenHeight - (smallRadius + bigRadius);
+        draw_gameboy(px, py, smallRadius, bigRadius, innerWidth, innerHeight, render_screen_width);
+
+        // background behind the screen
+        rl.drawRectangleRounded(
+            .{
+                .x = px + render_screen_width * 0.15,
+                .y = py + render_screen_width * 0.15,
+                .width = render_screen_width * 1.25,
+                .height = render_screen_height * 1.25,
+            },
+            0.1,
+            10,
+            .dark_gray,
+        );
+
+        rl.drawTexture(
+            fb_tex,
+            @intFromFloat(px + render_screen_width * 0.25),
+            @intFromFloat(py + render_screen_width * 0.25),
+            rl.Color.white,
+        );
+        // rl.drawFPS(0, 0);
+    }
 }
